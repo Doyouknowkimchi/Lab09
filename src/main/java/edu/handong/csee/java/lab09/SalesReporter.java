23fdcaf1cda0d215da1aeafde9a2d797c17378c2
Doyouknowkimchi <21700268@handong.edu>
2018-04-05 11:09:31 +0900

    first commit

---
+++ src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
@@ -0,0 +1,16 @@
+package edu.handong.csee.java.lab09;
+
+public class SalesReporter {
+	
+	private double highestSales;
+	private double averageSales;
+	private SalesAssociate[] team;
+	private int numberOfAssociate;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
