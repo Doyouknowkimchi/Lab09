85d57b1b736d46d3701a53b1e8d1cd8f22bbc11b
Doyouknowkimchi <21700268@handong.edu>
2018-04-10 19:07:07 +0900

    compute average and highestsales

--- src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
@@ -1,5 +1,23 @@
 package edu.handong.csee.java.lab09;
 
+
 public class SalesAssociate {
+	
+	private String name;
+	private double sales;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSales() {
+		return sales;
+	}
+	public void setSales(double sales) {
+		this.sales = sales;
+	}	
+	
 
 }
--- src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
@@ -1,16 +1,78 @@
 package edu.handong.csee.java.lab09;
 
+import java.util.Scanner;
+
 public class SalesReporter {
 	
-	private double highestSales;
+	private double highestsales;
 	private double averageSales;
 	private SalesAssociate[] team;
-	private int numberOfAssociate;
-	
+	private int numberOfAssociates; 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SalesReporter analyzer = new SalesReporter();//Where is MarketingAnalyser method? 
+				
+		analyzer.getData();
+		analyzer.computeAverage();
+		analyzer.computeHighestsales();
+		analyzer.printResult();
 	}
+		
+	public void getData() { //method that get data
+			
+			System.out.println("Enter the number of salesman: "); // send text to screen
+			Scanner myKeyboard = new Scanner(System.in); // readies the program for keyboard input
+			
+			int numOfSalesmen = myKeyboard.nextInt(); // get Salsemen's value
+			
+			team = new SalesAssociate[numOfSalesmen]; // set index
+			
+			for(int i=0; i < numOfSalesmen; i++) { // get salesmen's data by using for method
+				System.out.println("Enter data for associate number" + (i+1));
+				System.out.print("Name");
+				myKeyboard.nextLine();
+				String name = myKeyboard.nextLine();
+				System.out.print("Sales?");
+				double sales = myKeyboard.nextDouble();
+				team[i] = new SalesAssociate();
+				team[i].setName(name);
+				team[i].setSales(sales);
+				
+			}
+			
+	}
+
+	public void computeAverage() {
+		
+		double sum=0;
+		
+		for(int i=0; i<team.length; i++){
+			sum = sum + team[i].getSales();
+		}
+		averageSales = sum/team.length;
+	}
+			
+	public void computeHighestsales() {
+		highestsales = 0;
+		
+		for (int i = 0; i<team.length; i++) {
+			if(highestsales<team[i].getSales())
+			{
+				highestsales = team[i].getSales();
+			}
+			
+		}
+			
+				
+	}			
+	
+	
+	public void printResult(){
+		System.out.println("average : " + averageSales);
+		System.out.println("highestsales : " + highestsales);
+	
+	}
+
 
 }
