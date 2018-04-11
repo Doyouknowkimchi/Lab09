874186ef55479183a02fd8aa1cb773b4b5996f48
Doyouknowkimchi <21700268@handong.edu>
2018-04-11 22:13:54 +0900

    add code

--- src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
@@ -1,23 +1,25 @@
 package edu.handong.csee.java.lab09;
 
+/**
+ * 
+ * ready for another class
+ *
+ */
+public class SalesAssociate { //name of class
 
-public class SalesAssociate {
-	
-	private String name;
-	private double sales;
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public double getSales() {
-		return sales;
-	}
-	public void setSales(double sales) {
-		this.sales = sales;
-	}	
-	
+	private String name; // variable 1
+	private double sales; // variable 2
 
-}
+	public String getName() { //name of method
+		return name; //return name
+	}
+	public void setName(String name) {//name of method2
+		this.name = name; //set instance to input
+	}
+	public double getSales() { //name of method3
+		return sales; //return sale
+	}
+	public void setSales(double sales) { //name of method4
+		this.sales = sales; //set instance to input
+	}
+}	
